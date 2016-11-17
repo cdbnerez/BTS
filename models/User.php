39b5c11a542b261de60c1e20c52d46ec4e49b571
Carlos Daniel Nerez <cdbnerez@gmail.com>
2016-11-18 00:51:56 +0800

    Logged in using db credentials but has errors regarding authentication token

--- models/User.php
+++ models/User.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+
 
 /**
  * This is the model class for table "user".
@@ -17,7 +19,7 @@ use Yii;
  *
  * @property Logs[] $logs
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * @inheritdoc
@@ -62,4 +64,81 @@ class User extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Logs::className(), ['User_id' => 'id']);
     }
+
+    public static function findIdentity($id) {
+    $user = self::find()
+            ->where([
+                "id" => $id
+            ])
+            ->one();
+    if (!count($user)) {
+        return null;
+    }
+    return new static($user);
+}
+
+/**
+ * @inheritdoc
+ */
+public static function findIdentityByAccessToken($token, $userType = null) {
+
+    $user = self::find()
+            ->where(["accessToken" => $token])
+            ->one();
+    if (!count($user)) {
+        return null;
+    }
+    return new static($user);
+}
+
+/**
+ * Finds user by username
+ *
+ * @param  string      $username
+ * @return static|null
+ */
+public static function findByUsername($username) {
+    $user = self::find()
+            ->where([
+                "username" => $username
+            ])
+            ->one();
+    if (!count($user)) {
+        return null;
+    }
+    return new static($user);
+}
+
+/**
+ * @inheritdoc
+ */
+public function getId() {
+    return $this->id;
+}
+
+/**
+ * @inheritdoc
+ */
+public function getAuthKey() {
+    return $this->authKey;
+}
+
+/**
+ * @inheritdoc
+ */
+public function validateAuthKey($authKey) {
+    return $this->authKey === $authKey;
+}
+
+/**
+ * Validates password
+ *
+ * @param  string  $password password to validate
+ * @return boolean if password provided is valid for current user
+ */
+public function validatePassword($password) {
+    return $this->password === $password;
+}
+
+
 }
